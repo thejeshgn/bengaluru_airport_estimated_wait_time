2ef495f01b4a6333339320a477913c7a9e9756c2
Thejesh GN <i@thejeshgn.com>
2023-01-25 21:13:52 +0530

    added sql

--- query.sql
+++ query.sql
@@ -1,5 +1,44 @@
--- Get the data for a specific key
+select commit_at as "date_time", 
+        iv.key, 
+        iv.Queue_WaitDisplay,
+        iv.Queue_QueueLength,
+        iv.Desk_QueueLength,
+        iv.Desk_ProcessTime
+from 
+    item_version as iv, 
+    commits as c
+where 
+    iv._commit = c.id
+order by date_time asc
 
+-- Get the data for a specific key
+select commit_at as "date_time", 
+        iv.key, 
+        iv.Queue_WaitDisplay,
+        iv.Queue_QueueLength,
+        iv.Desk_QueueLength,
+        iv.Desk_ProcessTime
+from 
+    item_version as iv, 
+    commits as c
+where 
+    iv._commit = c.id and
+    iv.key = "T1IM"
+order by date_time asc
 
 
 -- Get the data for a specific day
+select commit_at as "date_time", 
+        iv.key, 
+        iv.Queue_WaitDisplay,
+        iv.Queue_QueueLength,
+        iv.Desk_QueueLength,
+        iv.Desk_ProcessTime
+from 
+    item_version as iv, 
+    commits as c
+where 
+    iv._commit = c.id and
+    iv.key = "T1IM" and
+    date_time = "2023-01-25%"
+
